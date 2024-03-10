ce88cb02cdf488872fa816f77fe9835d146bb14d
Diana Discher <dianadischer@dianas-mbp.mynetworksettings.com>
2024-03-10 13:41:18 -0400

    completed portfolio page, added images, and updated links and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import {Link, useLocation} from 'react-router-dom';
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 
 function Header() {
   const currentPage = useLocation().pathname;
+
+  const handleNavLinkClick = () => {
+    // Close the navbar when a nav link is clicked
+    const navbar = document.getElementById('responsive-navbar-nav');
+    if (navbar.classList.contains('show')) {
+      navbar.classList.remove('show');
+    }
+  };
 
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary sticky-top">
@@ -13,16 +22,32 @@ function Header() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
           <Nav>
-            <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'}>
+            <Link
+              to="/"
+              className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+              onClick={handleNavLinkClick}
+            >
               About
             </Link>
-            <Link to="/Portfolio" className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}>
+            <Link
+              to="/Portfolio"
+              className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
+              onClick={handleNavLinkClick}
+            >
               Portfolio
             </Link>
-            <Link to="/Contact" className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}>
+            <Link
+              to="/Contact"
+              className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+              onClick={handleNavLinkClick}
+            >
               Contact
             </Link>
-            <Link to="/Resume" className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}>
+            <Link
+              to="/Resume"
+              className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
+              onClick={handleNavLinkClick}
+            >
               Resume
             </Link>
           </Nav>
