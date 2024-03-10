e08c2fbc2b278218d6706ded4c87bdf6ef484881
Diana Discher <dianadischer@dianas-mbp.mynetworksettings.com>
2024-03-10 16:15:28 -0400

    updated styling and added resume, layout, and some text to resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,14 @@
+import resume from '../assets/Diana_Discher_Resume.pdf';
+
 export default function About() {
   return (
-    <div>
-      <h1>Resume</h1>
-      <p>
-        Here's a copy of my resume!
-      </p>
+    <div className='m-4'>
+      <div className="m-auto p-4 rounded" id="resume">
+        <h5 className='text-uppercase smallheader'>Skills</h5>
+        <h1 id="contact-header">My Experience and Toolkit</h1>
+        <p><a href={resume} target="_blank"> Click here</a> to see my resume. <br/>
+        These are the development tools I've used in previous projects:</p>
+      </div>
     </div>
   );
 }
